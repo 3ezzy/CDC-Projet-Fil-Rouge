98ee4f25b971ea049148c909e3a0e36fad69271c
Mehdi <khammali26@gmail.com>
2025-04-02 18:37:35 +0000

    Add authentication functionality with login and registration controllers, and update routes

--- Artisan_Pottery/routes/web.php
+++ Artisan_Pottery/routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +13,11 @@ Route::get('/', function () {
 
 
 Route::resource('categories', CategoryController::class);
-
 Route::resource('products', ProductController::class);
+
+
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
