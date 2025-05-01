c6fe96b4c2e1978f5497d5ff5a934088aaa6e067
Mehdi <khammali26@gmail.com>
2025-05-01 21:32:58 +0100

    personalize dashboard and navigation with user first name

--- Artisan_Pottery/resources/views/layouts/admin/navigation.blade.php
+++ Artisan_Pottery/resources/views/layouts/admin/navigation.blade.php
@@ -17,7 +17,7 @@
                 <span class="text-white font-bold">3E</span>
             </div>
             <div>
-                <h3 class="font-medium text-gray-800">3ezzy</h3>
+                <h3 class="font-medium text-gray-800">{{ Auth::user()->first_name }}</h3>
                 <p class="text-sm text-gray-500">Seller Account</p>
             </div>
         </div>
