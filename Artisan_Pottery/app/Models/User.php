2b38f7e8f31087b3914aa4e6489e34bc19454262
Mehdi <khammali26@gmail.com>
2025-05-02 23:06:08 +0100

    update fillable attributes in User model to include 'role'

--- Artisan_Pottery/app/Models/User.php
+++ Artisan_Pottery/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'first_name', 'last_name', 'name', 'email', 'password', 'phone', 'terms',
+        'first_name', 'last_name', 'role', 'email', 'password', 'phone', 'terms',
     ];
 
     /**
